627a0897e2b28c9ab0209cbf07af96b77a07be7c
Davi Dantas <davidantaslgamer@gmail.com>
2020-02-27 16:14:13 -0300

    primeiro commit

---
+++ mercadinho/src/test/java/com/mercainho/MercadinhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercainho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadinhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
